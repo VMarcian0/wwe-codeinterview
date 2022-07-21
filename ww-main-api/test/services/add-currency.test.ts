9d0036f6c101f9dbb89a03d391bb6e936da7a397
vmarcian0 <victor_marcian0@hotmail.com>
2022-07-20 21:30:29 -0300

    finished testing - going to start part 2

--- ww-main-api/test/services/add-currency.test.ts
+++ ww-main-api/test/services/add-currency.test.ts
@@ -1,10 +1,40 @@
 import assert from 'assert';
 import app from '../../src/app';
+import { AddCurrencyPayload, AddCurrencyPayloadCurrencyTypeKeys, AddCurrencyPayloadMethodKeys } from '../../src/types/add.currency.payload.type';
+import { UserType } from '../../src/types/user.type';
+import { WalletType } from '../../src/types/wallet.type';
 
 describe('\'addCurrency\' service', () => {
   it('registered the service', () => {
     const service = app.service('wallets/add-currency');
 
     assert.ok(service, 'Registered the service');
+  });
+  it('Update the value of a wallet', async () => {
+    const givenUser : UserType = {
+      email:'addcurrency@testing.com',
+      password:'1234567890'
+    };
+
+    const recievedUser = await app.services.users.create(givenUser) as UserType;
+
+    const userWallet = await app.services.wallets.find({query:{userId:recievedUser?.id as number},paginate:false}) as WalletType[];
+  
+    const wallet = userWallet[0];
+    assert.ok(wallet, 'Wallet needs to have an id');
+    assert.ok(wallet.soft_currency >= 10 && wallet.soft_currency <= 1000, 'Soft Currency must be within range [10-1000]');
+    assert.ok(wallet.hard_currency >= 5 && wallet.hard_currency <= 100, 'Soft Currency must be within range [5-100]');
+
+    // it should increment the value
+    const updateSoftWalletPayload : AddCurrencyPayload = {
+      currency_type: AddCurrencyPayloadCurrencyTypeKeys.SOFT,
+      method: AddCurrencyPayloadMethodKeys.ADD,
+      userId: recievedUser?.id as number,
+      value: 10
+    };
+
+    const updatedWallet = await app.services['wallets/add-currency'].create(updateSoftWalletPayload) as WalletType;
+
+    assert.ok(updatedWallet.soft_currency == (wallet.soft_currency+10) , 'Soft Currency must be within range [5-100]');
   });
 });
